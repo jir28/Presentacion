8931c138375d1e8a016051c8f16279a37d6f63d7
Jair Zamorano <87546129+jir28@users.noreply.github.com>
2022-04-18 19:35:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import showerS
 
 
 if __name__ == '__main__':
-    bulb = Bulb("ip de tu foco xiamoi yeelight")
-    bulb.turn_on()
-    while True:
-        tem = showerS.flujo()
-        if(tem==5):
-            sys.exit()
+    tem = showerS.flujo()
+    #bulb = Bulb("ip de tu foco xiamoi yeelight")
+    #bulb.turn_on()
+#while True:
+ #       tem = showerS.flujo()
+  #      if(tem==5):
+   #         sys.exit()
